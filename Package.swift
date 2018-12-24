83c28682ed23db4123f8363cfef6ae728087c33d
Pedro Piñera <pedro@ppinera.es>
2018-12-24 15:29:44 +0100

    Add products

--- Package.swift
+++ Package.swift
@@ -4,6 +4,10 @@ import PackageDescription
 
 let package = Package(
     name: "acho",
+    products: [
+      .library(name: "acho", targets: ["acho"]),
+      .library(name: "achoTesting", targets: ["achoTesting"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-package-manager.git", .revision("a107d28d1b40491cf505799a046fee53e7c422e1")),
         .package(url: "https://github.com/mtynior/ColorizeSwift.git", .upToNextMinor(from: "1.2.0")),
