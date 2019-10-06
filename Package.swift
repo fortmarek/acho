de2e67fa8e160079af0dd079db9d5e3ede18b2ee
Marek Fořt <marek.fort@ackee.cz>
2019-10-06 11:00:05 +0200

    Update SPM version to 5.0 release.

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .library(name: "achoTesting", targets: ["achoTesting"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-package-manager", .revision("a107d28d1b40491cf505799a046fee53e7c422e1")),
+        .package(url: "https://github.com/apple/swift-package-manager", .branch("swift-5.0-RELEASE")), 
         .package(url: "https://github.com/mtynior/ColorizeSwift.git", .revision("7492d039d594daccc39cacea343eed6cc9d1ed5a")),
     ],
     targets: [
